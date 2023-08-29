521db6c3cd12c23f0e0e362de2d234d6413d9e7f
Ifuhad622 <ifuhad007@gmail>
2023-08-29 15:02:21 -0700

    print_listint

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,68 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+/* Libraries */
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ * Return: 1 (Success), -1 (Error),
+ * and error set
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * struct listp_s - singly linked list
+ * @p: node pointers
+ * @next: next node point
+ *
+ * Description: singly linked pointer list
+ */
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+/* Mandatory tasks prototypes */
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int sum_listint(listint_t *head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+/* Advaned tasks prototypes */
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+void free_listp(listp_t **head);
+size_t free_listint_safe(listint_t **h);
+void free_listp2(listp_t **head);
+listint_t *find_listint_loop(listint_t *head);
+
+#endif /* end of lists.h file */
