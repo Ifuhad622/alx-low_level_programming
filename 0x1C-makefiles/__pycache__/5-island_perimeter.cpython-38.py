75868dff562d959dc2fb884c4e04cbf685af7646
Ifuhad622 <ifuhad007@gmail>
2024-02-21 17:42:45 -0800

    0x1C-makefiles

---
+++ 0x1C-makefiles/__pycache__/5-island_perimeter.cpython-38.py
@@ -0,0 +1,11 @@
+U 
+&#x0;&#x0;&#x0;&#x0;]}�a�&#x0;&#x0;�&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;@&#x0;&#x0;&#x0;s&#x0;&#x0;&#x0;d&#x0;Z&#x0;dd�&#x0;ZdS&#x0;)z/Defines an island perimeter measuring function.c&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;&#x0;C&#x0;&#x0;&#x0;s�&#x0;&#x0;&#x0;t&#x0;|&#x0;d&#x0;�}t&#x0;|&#x0;�}d}d}t|�D&#x0;]r}t|�D&#x0;]d}|&#x0;|&#x0;|&#x0;dkr0|d7&#x0;}|dkrp|&#x0;|&#x0;|d&#x0;&#x0;dkrp|d7&#x0;}|dkr0|&#x0;|d&#x0;&#x0;|&#x0;dkr0|d7&#x0;}q0q$|d&#x0;|d&#x0;&#x0;S&#x0;)z�Return the perimiter of an island.
+
+    The grid represents water by 0 and land by 1.
+
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
+    �&#x0;&#x0;&#x0;&#x0;�&#x0;&#x0;&#x0;�&#x0;&#x0;&#x0;�&#x0;&#x0;&#x0;)�len�range)�grid�widthZheightZedges�size�i�j�&#x0;r&#x0;&#x0;&#x0;�h/mnt/c/Users/user/Desktop/ALX/Gitprojects/alx-low_level_programming/0x1C-makefiles/5-island_perimeter.py�island_perimeter&#x0;&#x0;&#x0;s&#x0;&#x0;&#x0;&#x0;
+r&#x0;&#x0;&#x0;N)�__doc__r&#x0;&#x0;&#x0;r&#x0;&#x0;&#x0;r&#x0;&#x0;&#x0;r&#x0;&#x0;&#x0;r &#x0;&#x0;&#x0;�<module>&#x0;&#x0;&#x0;s&#x0;&#x0;&#x0;
